207df99a86f45fc1782df2aa398e68b93deb4af9
dasumit7 <sd3034734@gmail.com>
2025-12-11 15:04:20 +0600

    manage double paymentinfo added problem and all payments api added

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(cors())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kfapxri.mongodb.net/?appName=Cluster0`;
 
+function generateTrackingId() {
+  return "TRK-" + Math.random().toString(36).substring(2, 10).toUpperCase();
+}
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -27,94 +31,122 @@ async function run() {
 
     const db = client.db("issue_reporting_system");
     const issuesCollection = db.collection("issues");
-
+    const paymentsCollection = db.collection("payments");
     // issue api
     app.post('/issues', async (req, res) => {
-        const issue = req.body;
-        const result =  await issuesCollection.insertOne(issue);
-        res.send(result);
+      const issue = req.body;
+      issue.trackingId = generateTrackingId();
+      issue.priority = "Normal"; 
+      issue.paymentStatus = "unpaid";
+      const result = await issuesCollection.insertOne(issue);
+      res.send(result);
     });
     app.get('/issues/all', async (req, res) => {
-        const result =  await issuesCollection.find({}, { sort: { priority: -1 } }).toArray();
-        res.send(result);
+      const result = await issuesCollection.find({}, { sort: { priority: -1 } }).toArray();
+      res.send(result);
     });
-    app.get('/issues/:id', async (req, res)=>{
-      const {id} = req.params;
+    app.get('/issues/:id', async (req, res) => {
+      const { id } = req.params;
       const objectId = new ObjectId(id);
-      const result = await issuesCollection.findOne({_id: objectId});
+      const result = await issuesCollection.findOne({ _id: objectId });
       res.send(result);
     })
     app.get('/issues', async (req, res) => {
-        const email = req.query.email;
-        const query = {};
-        if(email){
-            query.reporterEmail = email;
-        }
-        const option = {
-            sort: { createdAt: -1 }}
-        const result =  await issuesCollection.find(query, option).toArray();
-        res.send(result);
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.reporterEmail = email;
+      }
+      const option = {
+        sort: { createdAt: -1 }
+      }
+      const result = await issuesCollection.find(query, option).toArray();
+      res.send(result);
     });
-    app.delete('/issues/:id', async (req, res)=>{
-      const {id} = req.params;
+    app.delete('/issues/:id', async (req, res) => {
+      const { id } = req.params;
       const objectId = new ObjectId(id);
-      const result = await issuesCollection.deleteOne({_id: objectId});
+      const result = await issuesCollection.deleteOne({ _id: objectId });
       res.send(result);
     })
-    app.put('/issues/:id', async (req, res)=>{
-      const {id} = req.params;
+    app.put('/issues/:id', async (req, res) => {
+      const { id } = req.params;
       const data = req.body;
       const objectId = new ObjectId(id);
       const update = {
         $set: data
       }
-      const result = await issuesCollection.updateOne({_id: objectId}, update);
+      const result = await issuesCollection.updateOne({ _id: objectId }, update);
       res.send(result);
     })
-// payment api
+    // payment api
     app.post('/create-payment-intent', async (req, res) => {
       const paymentInfo = req.body;
-      const amount = 79; 
+      const amount = 79;
       const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: 'USD',
-          unit_amount: amount,
-          product_data: {
-            name: `Issue boosting for Issue : ${paymentInfo.title}`,
+        line_items: [
+          {
+            price_data: {
+              currency: 'USD',
+              unit_amount: amount,
+              product_data: {
+                name: `Issue boosting for Issue : ${paymentInfo.title}`,
+              },
+            },
+            quantity: 1,
           },
+        ],
+        customer_email: paymentInfo.reporterEmail,
+        mode: 'payment',
+        metadata: {
+          issueId: paymentInfo.issueId,
+          name: paymentInfo.title,
+          trackingId: paymentInfo.trackingId
         },
-        quantity: 1,
-      },
-    ],
-    customer_email: paymentInfo.reporterEmail,
-    mode: 'payment',
-    metadata: {
-      issueId: paymentInfo.issueId,
-
-    },
-    success_url: `${process.env.SITE_DOMAIN}/issues/${paymentInfo.issueId}?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `${process.env.SITE_DOMAIN}/issues/${paymentInfo.issueId}?payment=failed`,
-  });
-      res.send({url: session.url});
+        success_url: `${process.env.SITE_DOMAIN}/issues/${paymentInfo.issueId}?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.SITE_DOMAIN}/issues/${paymentInfo.issueId}?payment=failed`,
+      });
+      res.send({ url: session.url });
     });
-    app.patch('/payment-success', async (req, res) =>{
+    app.patch('/payment-success', async (req, res) => {
       const sessionId = req.query.session_id;
       const session = await stripe.checkout.sessions.retrieve(sessionId);
-      if(session.payment_status === 'paid'){
+
+      const transactionId = session.payment_intent;
+      const query = {transactionId: transactionId};
+      const paymentExits = await paymentsCollection.findOne(query);
+
+      if (session.payment_status === 'paid' && !paymentExits) {
         const issueId = session.metadata.issueId;
-        const query = {_id: new ObjectId(issueId)};
+        const query = { _id: new ObjectId(issueId) };
         const update = {
           $set: {
-            paymentStatus : 'paid',
+            paymentStatus: 'paid',
             priority: 'High'
           }
         }
         const result = await issuesCollection.updateOne(query, update);
-        res.send(result);
+        const paymentRecord = {
+          amount: session.amount_total / 100,
+          currency: session.currency,
+          email: session.customer_email,
+          issueId: session.metadata.issueId,
+          issueName: session.metadata.name,
+          transactionId: session.payment_intent,
+          paymentStatus: session.payment_status,
+          paidAt: new Date(),
+          trackingId: session.metadata.trackingId
+        }
+        if (session.payment_status === 'paid') {
+          const paymentResult = await paymentsCollection.insertOne(paymentRecord);
+          res.send({ updateResult: result, paymentResult: paymentResult });
+        }
       }
     });
+    app.get('/all-payments', async(req, res) =>{
+      const result = await paymentsCollection.find({}, { sort: { paidAt: -1 } }).toArray();
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
